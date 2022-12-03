e3b7f3dc0f57d1aaf365a1625a95fe42e18ccb3c
oliver-wahome <olivernderitu135@gmail.com>
2022-12-03 22:22:06 +0300

    Twitter feed trends section complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//function to add a users details to firebase authentication
 function signUpSubmit(){
     var firstName = document.getElementById("signUpFirstName").value;
     var lastName = document.getElementById("signUpLastName").value;
@@ -27,9 +28,23 @@ function signUpSubmit(){
     );
 }
 
+/* 
+    function to check if a user has account in the firebase authentication 
+    database and either logging them in or alertin them to create an account.
+*/
 function loginSubmit(){
     var email = document.getElementById("loginEmail").value;
     var password = document.getElementById("loginPassword").value;
 
     console.log(email + " " + password);
+}
+
+//functions to change the trendsMoreBtn on hover of its parent div
+//function to change the trends more btn to blue on hover
+function moreBtnBlue(num) {
+    document.getElementById("moreImage"+num).src = "images/moreBlue.png";
+}
+//function to return trendsMoreBtn back to grey onmouseout
+function moreBtnGrey(num){
+    document.getElementById("moreImage"+num).src = "https://cdn-icons-png.flaticon.com/128/512/512142.png";
 }
