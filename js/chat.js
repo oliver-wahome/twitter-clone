c87a68d4e8c2a5293a529beb3d134f7e5006f200
oliver-wahome <olivernderitu135@gmail.com>
2022-12-22 14:16:17 +0300

    update: outputting all users to dmSection except logged in user

--- js/chat.js
+++ js/chat.js
@@ -13,6 +13,33 @@ firebase.auth().onAuthStateChanged((user) => {
                 document.getElementById("menuLeftProfileName").innerText = firstName +" "+ lastName;
                 document.getElementById("menuLeftProfileHandle").innerText = "@"+firstName+lastName;
             });
+
+        //getting all users and outputting them in dm section except logged in user
+        firebase.firestore().collection("users").where("userId", "!=", userId).get()
+            .then((allUsers) =>{
+                var content = "";
+
+                allUsers.forEach((doc) => {
+                    var username = doc.data().firstName + " " + doc.data().lastName;
+                    var handle = "@" + doc.data().firstName + doc.data().lastName;
+
+                    content += '<div class="tweet">';
+                        content += '<img class="tweetProfilePic" src="https://cdn-icons-png.flaticon.com/512/236/236831.png" alt="profile-picture-icon-image" />';
+                        content += '<div class="tweetContent">';
+                            content += '<div class="tweetContentTopRow">';
+                                content += '<div class="profileNames">';
+                                    content += '<p id="username">'+username+'</p>';
+                                    content += '<p id="userHandle">'+handle+'</p>';
+                                    content += '<div class="topRowDot"></div>';
+                                    content += '<p id="tweetTime">10h</p>';
+                                content += '</div>';
+                            content += '</div>';
+                        content += '</div>';
+                    content += '</div>';
+                });
+                $("#userDirectMessages").append(content);
+                
+            })
     }
     else {
         console.log("user is not logged in");
